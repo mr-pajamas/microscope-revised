b42c10d516796c9a43acd271d2e110c996e46d3b
汤力丞 <lctang@foxmail.com>
2017-12-29 13:50:03 +0800

    add post components

---
+++ imports/ui/components/post/post-list.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+
+import './post-item.js';
+import './post-list.html';
+
+const postData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  },
+];
+
+Template.postList.helpers({
+  posts() {
+    return postData;
+  }
+});
