195affffe1a93b1715bd0257ac7a9acc22538602
汤力丞 <lctang@foxmail.com>
2017-12-29 13:55:22 +0800

    Setup a `domain` helper on the `postItem`

--- imports/ui/components/post/post-item.js
+++ imports/ui/components/post/post-item.js
@@ -1,1 +1,11 @@
+import { Template } from 'meteor/templating';
+
 import './post-item.html';
+
+Template.postItem.helpers({
+  domain() {
+    const a = document.createElement('a');
+    ({ url: a.href } = this);
+    return a.hostname;
+  }
+});
